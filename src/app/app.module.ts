742d43320203bf956e76960faf6201ecdd0d6a6d
Oana Vezentan <oana.vezentan@gmail.com>
2017-09-28 10:07:25 +0200

    Modified app.modul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import {HttpClientModule} from '@angular/common/http';
 
@@ -15,7 +16,8 @@ import { BookFormComponent } from './book-form/book-form.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
