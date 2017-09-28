bf5f94153dc8c6850f6377629807e66c1d254f47
Oana Vezentan <oana.vezentan@gmail.com>
2017-09-28 03:37:26 +0200

    Added rxjs, observable and http imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { BookFormComponent } from './book-form/book-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookFormComponent
+    BookFormComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from '../book.model';
+import { Book }    from '../book.model';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'book-form',
+  providers: [],
   templateUrl: './book-form.component.html',
   styleUrls: ['./book-form.component.css']
 })
 export class BookFormComponent implements OnInit {
+
   model = new Book(1, '', '', 'http://');
-  newBook() {
-    return JSON.stringify(this.model);
-  }
+
   constructor() { }
 
   ngOnInit() {
@@ -18,8 +22,7 @@ export class BookFormComponent implements OnInit {
 
   submitted = false;
 
-  onSubmit() {
-    console.log("submit");
-    this.submitted = true;
-  }
+  onSubmit() { this.submitted = true; }
+
+  newBook() { return JSON.stringify(this.model); }
 }
